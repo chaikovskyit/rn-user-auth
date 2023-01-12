45dcd1bba7485eebeec7db1050797c06098f87a3
chaikovskyit <chaikovskyit@gmail.com>
2023-01-12 16:16:38 +0200

    added several components and screens

---
+++ constants/styles.js
@@ -0,0 +1,9 @@
+export const GlobalStyles = {
+  colors: {
+    primary100: "#f9beda",
+    primary500: "#c30b64",
+    primary800: "#610440",
+    error100: "#fcdcbf",
+    error500: "#f37c13",
+  },
+};
